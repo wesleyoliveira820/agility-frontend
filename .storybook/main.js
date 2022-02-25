a0d921883b9fddb749d0dc125b6ea6a825487f83
Wesley Oliveira <wesleyoliveiradeveloper@gmail.com>
2022-02-25 01:28:15 -0300

    chore: adds storybook addon for accessibility testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,12 @@
 module.exports = {
   core: {
-    bulder: 'webpack5',
+    builder: 'webpack5',
   },
   stories: ["../src/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"]
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y"
+  ]
 };
